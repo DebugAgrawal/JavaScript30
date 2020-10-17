d6c188916d5a0ce0f7535b113750c5dfe7d1b31a
Deepak Agrawal <debug.career@gmail.com>
2020-10-17 17:02:01 +0530

    commit

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -22,18 +22,36 @@ function updateButton(){
 
 function skip() {
     video.currentTime += parseFloat(this.dataset.skip);
-   }
+}
    
-   function handleRangeUpdate() {
-     video[this.name] = this.value;
-   }
+function handleRangeUpdate() {
+    video[this.name] = this.value;
+}
 
+function handleProgress() {
+    const percent = (video.currentTime / video.duration) * 100;
+    progressBar.style.flexBasis = `${percent}%`;
+  }
+
+function scrub(e) {
+    const scrubTime = (e.offsetX / progress.offsetWidth) * video.duration;
+    video.currentTime = scrubTime;
+  }
+  
 
 video.addEventListener('click',togglePlay);
 toggle.addEventListener('click',togglePlay);
 video.addEventListener('play',updateButton);
 video.addEventListener('pause',updateButton);
+
+video.addEventListener('timeupdate',handleProgress);
+
 toggle.addEventListener('click', togglePlay);
 skipButtons.forEach(button => button.addEventListener('click', skip));
 ranges.forEach(range => range.addEventListener('change', handleRangeUpdate));
 ranges.forEach(range => range.addEventListener('mousemove', handleRangeUpdate));
+
+progress.addEventListener('click', scrub);
+progress.addEventListener('mousemove',(e) => mousedown && scrub(e));
+progress.addEventListener('mousedown', () => mousedown = true);
+progress.addEventListener('mouseup', () => mousedown = false);
